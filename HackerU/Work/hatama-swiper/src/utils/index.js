3fe0e1719fad82951d55fa3815d9a16eacd752fa
Eliran Buskila <48204417+eliranBus@users.noreply.github.com>
2024-01-11 14:37:25 +0200

    Delete HackerU/Work/hatama-swiper directory

--- HackerU/Work/hatama-swiper/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-// export function incrementQuesNumber() {
-//   document.getElementsByClassName("swiper-button-next").addEventListener("click", function(){
-//     var url = new URL(location.href),
-//     dataUrl = '/q-2';
-    
-//     url.set( dataUrl ); 
-    
-//   });
-// }
