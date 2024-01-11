6510bc32d1bd00e45ce01e471b497ff243fb0aa8
eliranbus <eliran.bus@gmail.com>
2024-01-11 14:37:45 +0200

    add changes

--- Personal/Field and forest mushrooms/src/assets/reviews/reviews.js
+++ Personal/Field and forest mushrooms/src/assets/reviews/reviews.js
@@ -1,73 +1,5 @@
 export const englishReviews = [
   {
-    author: "Donna",
-    review:
-      "Eliran and his father were a pleasure to deal with. We needed a place to stay close to Gush Etzion for our daughters wedding and stayed 2 nights. The pictures truly reflect accurately the beauty and comfort of the rental. Getting ready we were a family of 7 plus bridesmaids, makeup artists, hairdressers, a photographer and videographer and all fit comfortably. We can't wait to see the photographs from the terrace which had the most magnificent view. It was the perfect setting for our event or if you just want a beautiful place to relax.",
-  },
-  {
-    author: "Inbal",
-    review:
-      "Amazing apartment, furnished and equipped down to the smallest detail! Great location, thin terrace with crazy views and really pleasant. We got exactly what we saw in the pictures and the hosts Eliran and Shimon took care of everything for us! It was amazing",
-  },
-  {
-    author: "Ehud",
-    review:
-      "Extremely nice, clean, new and equipped apartment. Really nice porch to sit, sip a glass of wine and watch the beautiful sunset over the mountains. Communication with Eliran were efficient. Thanks!",
-  },
-  {
-    author: "Tamara",
-    review:
-      "What an amazing stay! Above and beyond our expectations. Eliran’s place was stunning: pacious, bright, modern, stocked with all the touches and amenities needed, sparkling clean and in a perfect location for us. Everything was as listed. Comfortable beds, smooth wifi connection, amazing porch, view, coffee and much more. We were a family with three kids and our stay was two weeks. We wish we could have stayed longer. Check in/out were super easy and    flexible. Eliran was an exceptional host. Friendly, responsive, easy to communicate with and made sure we felt at home in every sense, which made the difference. Overall -I couldn’t be morec thankful with Eliran and his property. Highly recommend!",
-  },
-  {
-    author: "Meytal",
-    review:
-      "I arrived at the apartment with five more friends, we received an answer to every question even before arrival. The apartment was clean and beautiful, the balcony is perfect, the kitchen contains everything you need. Shimon and Eliran were available for everything. We had a lot of fun.",
-  },
-  {
-    author: "Havi",
-    review:
-      "Stunning new apartment! Everything is clean and well maintained Spacious, very pleasant and equipped with everything you need. Very kind host, took care of everything We needed, and even let us check out later. highly recommend!",
-  },
-  {
-    author: "Hagar",
-    review:
-      "We had a wonderful stay at Eliran’s. The apartment is very clean and pleasant, great for a family vacation. The communication with Eliran and his father, Shimon was very smooth and easy. They were both very accommodating and took care of our every need. The surroundings are beautiful and peaceful. We highly recommend this beautiful stay.",
-  },
-  {
-    author: "Jennifer",
-    review:
-      "Incredible views, beautiful home. You can watch sunset over the hills from the gorgeous wooden porch. Large windows let in sunlight in this lovely modern home that is tastefully decorated. Host is friendly and welcoming!",
-  },
-  {
-    author: "Mendy",
-    review:
-      "Great, clean place. Eliran was very patient answering every question quickly and very accommodating.",
-  },
-  {
-    author: "Naama",
-    review:
-      "We arrived a group of friends and really enjoyed the apartment. The apartment was very clean and looked like in the pictures, the balcony was big and cozy and Eliran and his father took care of everything we needed. warmly recommended.",
-  },
-  {
-    author: "Elisheva",
-    review:
-      "The hospitality was really nice, the apartment was new and very clean, and the communication with Eliran was really nice.",
-  },
-  {
-    author: "Keren",
-    review:
-      "The place is very beautiful and organized. There is everything and nothing is missing. The landlord was nice and kind!",
-  },
-  {
-    author: "Mark",
-    review:
-      "An absolutely stunning property, we had two absolutely gorgeous sunsets, it's been very nicely decorated and the owner was very friendly and accommodating. I would highly recommend.",
-  },
-];
-
-export const hebrewReviews = [
-  {
     author: "מקום הופעת פטריות - באמונות ובאגדות",
     review:
       "כבר מהעת העתיקה התייחסו לפטריות בחשד ופחד, ומכאן צמחו אמונות טפלות ואגדות.      לתופעות שהאדם לא ידע להסבירן הומצאו הסברים שונים ומשונים, כמו גם בנושא עטלפים,      עופות לילה ונחשים. גורם ההפתעה של ינשוף או של נחש, שפעילים בלילה הפחיד במיוחד. את אוכלוסיות האדם חסרי ההשכלה והידע. בפטריות - הרעלים ומגוון ההשפעות על מי שאכלן הולידו אגדות וספורים במיוחד בגלל תופעת ההזיות. למקום הצצת פטריות יש מספר אמונות, לדוגמא: כשסוסים דוהרים בין העצים ויוצא להם קצף מעורב בדם - שנה לאחר מכן צומחות פטריות אדומות עם נקודות לבנות (אמנית זבובנית). במקום שצבאים הזדווגו, צורת ביציהם המוזרות וריחן דוחה - צצות פטריות שצורתן פאלית (פינית מבאישה). היכן שזקנה הלכה בשדה ופגשה בשטן - צצות פטריות 'מקומטות' כעור זקן (גמצוץ). בחורף, הופיעו ברקים ורעמים, האנרגיה התפזרה מסביב - צצות פטריות במעגל. כ''כ בספור אחר - מופיע מעגל פטריות - במקום בו רקדו פיות. ברקים הם גם הגורם המבקיע את קרום הקרקע ומתחת לסדק מתחבאת פטריה (כמהין). במקום שכלב השתין - למחרת צצות פטריות.",
@@ -76,5 +8,28 @@ export const hebrewReviews = [
     author: "מינהגים ואמונות בתרבויות שונות",
     review:
       "השימוש בפטריות, בעיקר בשבטים קדומים, עבר במסורת מדור לדור.הם הכירו את תכונות הפטריות והשפעתן המזיקה,ומכאן נהגו להשתמש למטרותיהם. אמנית זבובנית -  המשקה הנרקוטי הפופולרי אצל האסקימוסים היה שתן של אדם שאכל ממנה. הויקינגים אכלו מעט ממנה לקראת קרבות אכזריים.ההשראה לתלבושתו של סנטה קלאוס, בצבעי אדום לבן - באה מהפטריה היפה. פטריות רעילות - ציידים נהגו למרוח את קצה החץ ששימש לצייד כדי לטשטש את החיה הניצודה. פסיפנית החולות - בעת הבשלה, הכמות האדירה של הנבגים החומים, שימשו להכנת צבע לבדים. החומר פסילוסיבין המצוי בה גורם להזיות קשות, וגם בימינו יש בו שימוש.",
+  },
+  {
+    author: "מעגלי הפיות",
+    review:
+      "מעגלי הפיות,השדים והמכשפות. מאז המאה ה 13 ידועה תופעה מרשימה שהעסיקה רבים - פטריות הצצות בסדור מעגלי בחורש ,ביער,ויותר-בעשביה ובדשאים. הסיבה שלא היה הסבר לתופעה-הולידה אגדות אמונות וספורים שונים ומוזרים, מתקופות קדומות ועד ימינו. בכל ארץ או קהילה האמינו באופן שונה ומיסטי שנוכל לראות ביצירות אמנות עתיקות. המוטיב המרכזי נסוב על ריקוד מעגלי שבוצע במקום ע''י מכשפות,פיות או שדים, שהצמיח פטריות הבאות ונעלמות תוך זמן קצר. מהדוגמאות שנאספו ניתן להבחין שבאנגליה-היתה זו פייה, בגרמניה-מכשפה ובהולנד-שד. מה אומרים המיקולוגים? ידועים כ 60 מיני פטריות היוצרים מעגלים בקוטר של 10-20 מטרים ובמקרה נדיר נמדדו 600 מ' במקום מסוים שנמשך מאות שנים. המעגל לא בהכרח מושלם בצורתו, יש גם קשת או קווים אלכסוניים כלשהם. מדי שנה מתרחב המעגל בממצע של 20 ס''מ עד שניתקל בגורם מפריע, למשל מעגל של מין אחר או שנוי במרכיב הקרקע שאינו מתאים לו. מינים ספציפיים הנכללים בתופעה- שייכים לסוג פקועה,אמנית,ישעורית,משפכנית ועוד. בישראל מוכרת למדי ירוקת הדפים על דשאים בסוף הקיץ, ומשפכנית גדולה ביער בחרף. כיצד נוצר מעגל? נבג נובט בקרקע, מתפתחים תאים ברשת קורים, הנקראים תפטיר.במינים המיוחדים גדל התפטיר רק לכוון אחד,כלפי חוץ,ובכל שנה מת החלק הוותיק. כך, עם השנים, הולך ומתרחב המעגל.חמרי המזון מנוצלים עד תום והתפטיר ממשיך לחלק מזין שבשוליים. במועד ובתנאים המתאימים-צומחים גופי הרביה, שהן הפטריות, מעל לפני השטח. לאחר שנים המקום שהיה דל במזון יכול להתחדש. גננים, המגדלים חלקות דשא, נהנים אמנם מהמראה היפה של מעגל הפטריות, אבל מודאגים וסובלים מהתוצאה-אם יש צפיפות-תפטיר חל נוון ומוות של הדשא. הפתרון הוא אוורור המקום בכלים אגרוטכניים. לסכום - אם ראיתם מעגל שכזה -בידקו בשנההבאה באותו מקום וראו מה קוטרו הפעם.",
+  },
+];
+
+export const hebrewReviews = [
+  {
+    author: "מקום הופעת פטריות - באמונות ובאגדות",
+    review:
+      "כבר מהעת העתיקה התייחסו לפטריות בחשד ופחד, ומכאן צמחו אמונות טפלות ואגדות. לתופעות שהאדם לא ידע להסבירן הומצאו הסברים שונים ומשונים, כמו גם בנושא עטלפים, עופות לילה ונחשים. גורם ההפתעה של ינשוף או של נחש, שפעילים בלילה הפחיד במיוחד. את אוכלוסיות האדם חסרי ההשכלה והידע. בפטריות - הרעלים ומגוון ההשפעות על מי שאכלן הולידו אגדות וספורים במיוחד בגלל תופעת ההזיות. למקום הצצת פטריות יש מספר אמונות, לדוגמא: כשסוסים דוהרים בין העצים ויוצא להם קצף מעורב בדם - שנה לאחר מכן צומחות פטריות אדומות עם נקודות לבנות (אמנית זבובנית). במקום שצבאים הזדווגו, צורת ביציהם המוזרות וריחן דוחה - צצות פטריות שצורתן פאלית (פינית מבאישה). היכן שזקנה הלכה בשדה ופגשה בשטן - צצות פטריות 'מקומטות' כעור זקן (גמצוץ). בחורף, הופיעו ברקים ורעמים, האנרגיה התפזרה מסביב - צצות פטריות במעגל. כ''כ בספור אחר - מופיע מעגל פטריות - במקום בו רקדו פיות. ברקים הם גם הגורם המבקיע את קרום הקרקע ומתחת לסדק מתחבאת פטריה (כמהין). במקום שכלב השתין - למחרת צצות פטריות.",
+  },
+  {
+    author: "מינהגים ואמונות בתרבויות שונות",
+    review:
+      "השימוש בפטריות, בעיקר בשבטים קדומים, עבר במסורת מדור לדור.הם הכירו את תכונות הפטריות והשפעתן המזיקה,ומכאן נהגו להשתמש למטרותיהם. אמנית זבובנית - המשקה הנרקוטי הפופולרי אצל האסקימוסים היה שתן של אדם שאכל ממנה. הויקינגים אכלו מעט ממנה לקראת קרבות אכזריים. ההשראה לתלבושתו של סנטה קלאוס, בצבעי אדום לבן - באה מהפטריה היפה. פטריות רעילות - ציידים נהגו למרוח את קצה החץ ששימש לצייד כדי לטשטש את החיה הניצודה. פסיפנית החולות - בעת הבשלה, הכמות האדירה של הנבגים החומים, שימשו להכנת צבע לבדים. החומר פסילוסיבין המצוי בה גורם להזיות קשות, וגם בימינו יש בו שימוש.",
+  },
+  {
+    author: "מעגלי הפיות",
+    review:
+      "מעגלי הפיות,השדים והמכשפות. מאז המאה ה 13 ידועה תופעה מרשימה שהעסיקה רבים - פטריות הצצות בסדור מעגלי בחורש, ביער,ויותר-בעשביה ובדשאים. הסיבה שלא היה הסבר לתופעה-הולידה אגדות אמונות וספורים שונים ומוזרים, מתקופות קדומות ועד ימינו. בכל ארץ או קהילה האמינו באופן שונה ומיסטי שנוכל לראות ביצירות אמנות עתיקות. המוטיב המרכזי נסוב על ריקוד מעגלי שבוצע במקום ע''י מכשפות, פיות או שדים, שהצמיח פטריות הבאות ונעלמות תוך זמן קצר. מהדוגמאות שנאספו ניתן להבחין שבאנגליה-היתה זו פייה, בגרמניה-מכשפה ובהולנד-שד. מה אומרים המיקולוגים? ידועים כ 60 מיני פטריות היוצרים מעגלים בקוטר של 10-20 מטרים ובמקרה נדיר נמדדו 600 מ' במקום מסוים שנמשך מאות שנים. המעגל לא בהכרח מושלם בצורתו, יש גם קשת או קווים אלכסוניים כלשהם. מדי שנה מתרחב המעגל בממצע של 20 ס''מ עד שניתקל בגורם מפריע, למשל מעגל של מין אחר או שנוי במרכיב הקרקע שאינו מתאים לו. מינים ספציפיים הנכללים בתופעה- שייכים לסוג פקועה,אמנית, ישעורית, משפכנית ועוד. בישראל מוכרת למדי ירוקת הדפים על דשאים בסוף הקיץ, ומשפכנית גדולה ביער בחרף. כיצד נוצר מעגל? נבג נובט בקרקע, מתפתחים תאים ברשת קורים, הנקראים תפטיר. במינים המיוחדים גדל התפטיר רק לכוון אחד, כלפי חוץ, ובכל שנה מת החלק הוותיק. כך, עם השנים, הולך ומתרחב המעגל. חמרי המזון מנוצלים עד תום והתפטיר ממשיך לחלק מזין שבשוליים. במועד ובתנאים המתאימים-צומחים גופי הרביה, שהן הפטריות, מעל לפני השטח. לאחר שנים המקום שהיה דל במזון יכול להתחדש. גננים, המגדלים חלקות דשא, נהנים אמנם מהמראה היפה של מעגל הפטריות, אבל מודאגים וסובלים מהתוצאה-אם יש צפיפות-תפטיר חל נוון ומוות של הדשא. הפתרון הוא אוורור המקום בכלים אגרוטכניים. לסכום - אם ראיתם מעגל שכזה -בידקו בשנההבאה באותו מקום וראו מה קוטרו הפעם.",
   },
 ];
